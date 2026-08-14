81ff65bfea71984a826f30a6b5bebfcddf36521e
AK-junior <codeninjak27@gmail.com>
2026-08-14 10:02:23 +0530

    Submitting Program -1

---
+++ Lab-01/VariableandIdentifierPractice.py
@@ -0,0 +1,18 @@
+# Section C: Programs to Write
+# 1. Variable and Identifier Practice
+#Declare variables for your name, age, height and whether you are a student
+#(True/False). Print each variable along with its type using type().
+
+name = "Aishwarya Kumar Singh"
+age = 24
+height = 170
+isStudent = True
+print("Name : ",name,type(name))
+print("Age : ",age,type(age))
+print("Height : ",height,type(height))
+print("Whether Student : ",isStudent,type(isStudent))
+
+# o/p :
+#(base) ak_junior@Aishwaryas-MacBook-Air Lab-01 % python VariableandIdentifierPractice.py
+#Name :  Aishwarya Kumar Singh <class 'str'>
+#Whether Student :  True <class 'bool'>
