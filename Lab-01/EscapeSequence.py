7153a2ea506eec2067b5ee391b904e3d7c4ee55f
AK-junior <codeninjak27@gmail.com>
2026-08-14 10:05:58 +0530

    Submitting Program -6

---
+++ Lab-01/EscapeSequence.py
@@ -0,0 +1,20 @@
+#6. Escape Sequence Practice
+#Write a short program that prints a small receipt or table like output 
+#using \t and \n to align text neatly, for example an item name 
+#and its price on each line.
+
+print("Item Name\tMRP\tBilling Price\n")
+print("Keyboard\t899\t\t749\n")
+print("Mouse\t\t499\t\t449\n")
+print("Monitor\t\t10,000\t\t9,499\n")
+
+
+#(base) ak_junior@Aishwaryas-MacBook-Air Lab-01 % python EscapeSequence.py
+#Item Name	MRP	    Billing Price
+
+#Keyboard	899		    749
+
+#Mouse		499		    449
+
+#Monitor   10,000		9,499
+
