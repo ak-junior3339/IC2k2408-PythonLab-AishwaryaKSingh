240f10743b3dcff8ba1851413e6dd4ffe41d046d
AK-junior <codeninjak27@gmail.com>
2026-08-14 10:01:03 +0530

    Submitting Section B py file

---
+++ Lab-01/SectionB.py
@@ -0,0 +1,15 @@
+print(2 + 3 * 4 - 1)                  
+print(10 % 3 + 2 ** 2)             
+print((5 + 3) / 2 == 4)            
+print(type(5 / 2))                   
+print("Python"[1:4])                 
+print("Hi\tThere\nBye")           
+
+# (base) ak_junior@Aishwaryas-MacBook-Air Lab-01 % python SectionB.py 
+# 13
+# 5
+# True
+# <class 'float'>
+# yth
+# Hi	There
+# Bye
